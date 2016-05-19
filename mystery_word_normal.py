2f307324fb50279e0b1e6ea24cd88b098857c6fd
emilycharris <emily.harris.mail@gmail.com>
2016-05-19 13:55:58 -0400

    first round of code

---
+++ mystery_word_normal.py
@@ -0,0 +1,29 @@
+import random
+
+#The computer must select a word at random from the list of words in the file
+
+with open("words") as opened_file:
+    words = opened_file.read().split("\n")
+
+secret_word = random.choice(words).lower()
+#print(secret_word)
+
+#Let the user know how many letters the computer's word contains.
+print("Let's get started. The secret word contains " + str(len(secret_word)) + " letters.")
+
+#Ask the user to supply one guess (i.e. letter) per round.
+#This letter can be upper or lower case and it should not matter.
+#Assume the user will only submit one letter (see Hard Mode for more on this.)
+guess = input("Guess a letter: ").lower()
+print(guess)
+
+#Let the user know if their guess appears in the computer's word.
+
+#Display the partially guessed word, as well as letters that have not been guessed.
+#A user is allowed 8 guesses. Remind the user of how many guesses they have left after each round.
+#A user loses a guess only when they guess incorrectly.
+#If they guess a letter that is in the computer's word, they do not lose a guess.
+#If the user guesses the same letter twice, do not take away a guess.
+#Instead, print a message letting them know they've already guessed that letter and ask them to try again.
+#The game should end when the user constructs the full word or runs out of guesses.
+#If the player runs out of guesses, reveal the word to the user when the game ends.
